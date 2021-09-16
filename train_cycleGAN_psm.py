04a8f3d20d030b459066d75ac5be73884ce9a490
VictorTao1998 <jit124@ucsd.edu>
2021-09-16 10:26:12 -0700

    a

--- train_cycleGAN_psm.py
+++ train_cycleGAN_psm.py
@@ -78,8 +78,6 @@ def train(gan_model, psmnet_model, feaex, TrainImgLoader, ValImgLoader):
         #avg_train_scalars_psmnet = AverageMeterDict()
         for batch_idx, sample in enumerate(TrainImgLoader):
             print('iter: ', batch_idx)
-            if batch_idx > 5:
-                break
             global_step = (len(TrainImgLoader) * epoch_idx + batch_idx) * cfg.SOLVER.BATCH_SIZE
             if global_step > cfg.SOLVER.STEPS:
                 break
