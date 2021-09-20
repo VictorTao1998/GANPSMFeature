19b1c847dd6db1d6fb150f118e0b180eff5a5b39
VictorTao1998 <jit124@ucsd.edu>
2021-09-20 12:05:45 -0700

    a

--- run_psm_train.sh
+++ run_psm_train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PYTHONWARNINGS="ignore"
 
-python -m torch.distributed.launch /cephfs/jianyu/GANPSMFeature/train_psm.py --config-file /cephfs/jianyu/GANPSMFeature/configs/remote_train_gan.yaml --summary-freq 500 --save-freq 100 --logdir /cephfs/jianyu/eval/ganpsm_pre2
+python -m torch.distributed.launch /cephfs/jianyu/GANPSMFeature/train_psm.py --config-file /cephfs/jianyu/GANPSMFeature/configs/remote_train_gan.yaml --summary-freq 500 --save-freq 100 --logdir /cephfs/jianyu/eval/ganpsm_pre3
