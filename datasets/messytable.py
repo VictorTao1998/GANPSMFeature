ba9dad25b3b2ce9565f2f81ab90ab751ab5f6390
VictorTao1998 <jit124@ucsd.edu>
2021-09-21 11:39:57 -0700

    a

--- datasets/messytable.py
+++ datasets/messytable.py
@@ -161,7 +161,7 @@ class MessytableDataset(Dataset):
         img_R_rgb = (img_R_rgb - 0.5) / 0.5
         img_sim_rgb = (img_sim_rgb - 0.5) / 0.5
         # random crop the image to 256 * 512
-        print(img_L_rgb.shape, img_R_rgb.shape, img_sim_rgb.shape)
+        #print(img_L_rgb.shape, img_R_rgb.shape, img_sim_rgb.shape)
         h, w = img_L_rgb.shape[1:]
         th, tw = cfg.ARGS.CROP_HEIGHT, cfg.ARGS.CROP_WIDTH
         x = random.randint(0, h - th)
@@ -180,7 +180,7 @@ class MessytableDataset(Dataset):
         img_sim_rgb = img_sim_rgb[:,2*x: 2*(x+th), 2*y: 2*(y+tw)]  # real original res in 1080*1920
 
         #img_L_rgb, img_R_rgb, img_sim_rgb = process(img_L_rgb), process(img_R_rgb), process(img_sim_rgb)
-
+        print(img_L_rgb.shape, img_R_rgb.shape, img_disp_l.shape, img_depth_l.shape, img_sim_rgb.shape)
         
 
         item = {}
