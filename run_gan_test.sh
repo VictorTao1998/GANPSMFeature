16da563de29fe8a4657d865a8880af4de8d9d7e0
VictorTao1998 <jit124@ucsd.edu>
2021-09-22 17:59:46 -0700

    a

---
+++ run_gan_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export PYTHONWARNINGS="ignore"
+
+python -m torch.distributed.launch /cephfs/jianyu/GANPSMFeature/test_cycleGAN_psm.py --config-file /cephfs/jianyu/GANPSMFeature/configs/remote_train_gan.yaml --summary-freq 100 --save-freq 100 --logdir /cephfs/jianyu/eval/ganpsm_test
