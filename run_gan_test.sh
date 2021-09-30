aa1994c596ccb4cb2359b946c400199245541246
VictorTao1998 <jit124@ucsd.edu>
2021-09-29 20:19:25 -0700

    test

--- run_gan_test.sh
+++ run_gan_test.sh
@@ -4,4 +4,4 @@ export PYTHONWARNINGS="ignore"
 python -m torch.distributed.launch /cephfs/jianyu/GANPSMFeature/test_cycleGAN_psm.py \
 --config-file /cephfs/jianyu/GANPSMFeature/configs/remote_test_gan.yaml --output /cephfs/jianyu/eval/ganpsm_test_deep_sim \
 --model /cephfs/jianyu/eval/GANPSMFeature_deep/models/model_49999.pth --gan-model /cephfs/jianyu/eval/GANPSMFeature_deep/models/model_49999.pth \
---exclude-bg --exclude-zeros
+--exclude-bg
