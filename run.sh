bfdf6f77ce3d9a9609ea78101ecc99eecaf38358
VictorTao1998 <jit124@ucsd.edu>
2021-10-03 02:26:59 -0700

    test

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PYTHONWARNINGS="ignore"
 
-python -m torch.distributed.launch /cephfs/jianyu/GANPSMFeature/train_cycleGAN_psm.py --config-file /cephfs/jianyu/GANPSMFeature/configs/remote_train_gan.yaml --summary-freq 100 --save-freq 2000 --logdir /cephfs/jianyu/eval/GANPSMFeature_deep --loadmodel "/cephfs/jianyu/eval/psm_deep_train/models/model_best.pth"
+python -m torch.distributed.launch /cephfs/jianyu/GANPSMFeature/train_cycleGAN_psm.py --config-file /cephfs/jianyu/GANPSMFeature/configs/remote_train_gan.yaml --summary-freq 100 --save-freq 2000 --logdir /cephfs/jianyu/eval/ganpsm_trains_deep --loadmodel "/cephfs/jianyu/eval/psm_deep_train/models/model_best.pth"
