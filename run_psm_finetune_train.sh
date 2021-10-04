ce8a9919f4b7f1396bca6c9c613c6269dbc88ff2
VictorTao1998 <jit124@ucsd.edu>
2021-10-04 16:22:53 -0700

    finetune

--- run_psm_finetune_train.sh
+++ run_psm_finetune_train.sh
@@ -3,5 +3,5 @@ export PYTHONWARNINGS="ignore"
 
 python -m torch.distributed.launch /cephfs/jianyu/GANPSMFeature/train_finetune_psm.py \
 --config-file /cephfs/jianyu/GANPSMFeature/configs/remote_train_gan.yaml --summary-freq 1000 \
---save-freq 100 --logdir /cephfs/jianyu/eval/psm_deep_train \
+--save-freq 100 --logdir /cephfs/jianyu/eval/psm_deep_train_finetune \
 --loadmodel "/cephfs/jianyu/eval/psm_deep_train/models/model_best.pth"
