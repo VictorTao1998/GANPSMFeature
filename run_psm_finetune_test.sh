dac579b835290ab00bb0209433aa1312f7b72dd5
VictorTao1998 <jit124@ucsd.edu>
2021-10-07 12:56:53 -0700

    test

--- run_psm_finetune_test.sh
+++ run_psm_finetune_test.sh
@@ -2,6 +2,6 @@
 export PYTHONWARNINGS="ignore"
 
 python -m torch.distributed.launch /cephfs/jianyu/GANPSMFeature/test_psm_finetune.py \
---config-file /cephfs/jianyu/GANPSMFeature/configs/remote_test_gan.yaml --output /cephfs/jianyu/eval/psm_finetune_test \
---model /cephfs/jianyu/eval/psm_deep_train_finetune/models/model_best.pth \
+--config-file /cephfs/jianyu/GANPSMFeature/configs/remote_test_gan.yaml --output /cephfs/jianyu/eval/psm_finetune_test_ori \
+--model /cephfs/jianyu/eval/psm_deep_train_finetune_ori/models/model_best.pth \
 --exclude-bg --exclude-zeros --onreal
